f780076c9fa47ff1d3dc464f0bb0866d0244581a
onelei <ahleiwolong@163.com>
2022-06-11 16:43:36 +0800

    init
    init

---
+++ Assets/HelloWorld/Runtime/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Onelei.HelloWorld
+{
+    public class HelloWorld : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("Hello World");
+        }
+    }
+}
